7848c2ef0d89f01ad527a20873aee970262d7113
ddhruvbajaj <95019483+ddhruvbajaj@users.noreply.github.com>
2022-07-14 12:51:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(500,460);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function snap_shot(){
+    save('filtercolor.png');
+}
